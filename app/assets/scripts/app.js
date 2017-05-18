ac43b27146b0c0c636bc21eebd337a8bac40d179
roni0101 <roni94radev@gmail.com>
2017-05-18 15:49:35 +0200

    Added templating and check is user logged in

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -4,10 +4,36 @@ var App = (function () {
 	function init() {
 		console.log('Init App');
 
-		// Append events to layout
+		// Get event template and events data from Preloader
+		var tempEvent = Preload.getSource('event-item.html');
+		var dataEvents = Preload.getSource('events.json');
+
+
+		// Populate template with data
+		dataEvents.forEach(function (event) {
+			
+			var layoutEvent = tempEvent.replace('{{ NAME }}', event.name);
+
+			// Append to list 
+			
+		});
+
 		// Check if user is loged in
-		// Display data based on that 
+
+
+		if( localStorage.eventAcc ){
+
+			var userAccout = JSON.parse( localStorage.eventAcc );
+
+			if(userAccout.logedin == "yes"){
+				console.log("loged in");
+				// Append User status
+				// Display data based on that 
+			}
+		}
+
 		// Run app
+		
 	}
 
 	function run() {
